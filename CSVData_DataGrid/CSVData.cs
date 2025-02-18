a5345f0f40646f1fd8402874aed5cf9ca02be89b
decha konklong <decha328769@gmail.com>
2025-02-18 17:27:46 +0700

    Add project files.

---
+++ CSVData_DataGrid/CSVData.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualBasic.FileIO;
+using System.IO;
+using System.Windows.Xps.Serialization;
+
+namespace CSVData_DataGrid
+{
+    internal class CSVData
+    {
+        public static DataView GetCsvData(string path)
+        {
+            DataTable dataTable = new DataTable();
+            TextFieldParser parser = new TextFieldParser(path);
+            parser.SetDelimiters(",");
+
+            if (!parser.EndOfData)
+            {
+                var columns = parser.ReadFields();
+                foreach (var col in columns)
+                {
+                    dataTable.Columns.Add(col);
+                }
+
+            }
+
+            while (!parser.EndOfData)
+            {
+                var row = parser.ReadFields();
+                dataTable.Rows.Add(row);
+
+            }
+
+            return dataTable.DefaultView;
+
+
+        }
+    }
+}
---
+++ CSVData_DataGrid/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Win32;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.VisualBasic.FileIO;
+
+namespace CSVData_DataGrid
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {    
+              // 1 open dialog window
+            OpenFileDialog openFile = new OpenFileDialog();
+              // 2 filter check CSV only 
+            openFile.Filter = "Cav Files| *.csv";
+             
+           
+                 //Check if have data from dialog 
+            if (openFile.ShowDialog() == true)
+            {   
+                 // Receive value from Class GetCsvData 
+                 // Access to CSVData stucture and GetCsvData funtion and pass value openFile.FileName 
+                var csvData = CSVData.GetCsvData(openFile.FileName);
+
+                 // Get data csv to Name CsvDataGrid XAML 
+                CsvDataGrid.ItemsSource = csvData;
+            }
+
+
+        }
+
+
+    }
+}
